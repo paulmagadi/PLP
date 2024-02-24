47e91d3d1d45eb5a936632161c662ade9d23109a
SQCodes <paulsqmagadi@gmail.com>
2024-02-24 12:15:14 +0300

    new file:   Python_Programming/user_input.py

---
+++ Python_Programming/user_input.py
@@ -0,0 +1,11 @@
+
+name = input("What is your name? ")
+
+
+age = input("What is your age? ")
+
+
+location = input("Where do you live? ")
+
+
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
