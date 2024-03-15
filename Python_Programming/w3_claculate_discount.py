f32fd717d238dd70395315c4615a06eef51001e2
SQCodes <paulsqmagadi@gmail.com>
2024-03-15 11:34:40 +0300

    modified:   Python_Programming/w3_claculate_discount.py

--- Python_Programming/w3_claculate_discount.py
+++ Python_Programming/w3_claculate_discount.py
@@ -18,3 +18,19 @@ if final_price != original_price:
     print(f"The final price after applying {discount_percent}% discount is: ${final_price:.2f}")
 else:
     print("No discount applied. The original price remains: ${:.2f}".format(original_price))
+
+
+list1 = [3 , 2 , 5 , 6 , 0 , 7, 9]
+
+sum = 0
+sum1 = 0
+for elem in list1:
+
+    if (elem % 2 == 0):
+        sum = sum + elem
+    continue
+
+if (elem % 3 == 0):
+    sum1 = sum1 + elem
+
+print(sum , end=" ")
