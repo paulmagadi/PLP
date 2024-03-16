e0ba8975911a7488a89aee4a8555eaa343e4c55b
SQCodes <paulsqmagadi@gmail.com>
2024-03-16 11:31:48 +0300

    new file:   hackerthon/hackerthon_dart1.dart 	new file:   hackerthon/hackerthon_python.py

---
+++ hackerthon/hackerthon_dart1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Paul";
+  int age = 25;
+  String school = "Power Learn Project School";
+  String hobby = "coding";
+
+  print("My name is $name. I am $age years old, studying at $school, and my hobby is $hobby.");
+}
---
+++ hackerthon/hackerthon_python.py
@@ -0,0 +1,51 @@
+# Functions
+def fibonacci(n):
+    """
+    This function generates the Fibonacci sequence up to a specified term n using iteration.
+
+    Args:
+        n: The number of terms in the Fibonacci sequence.
+
+    Returns:
+        A list containing the Fibonacci sequence up to n terms.
+    """
+    if n <= 1:
+        return n
+    else:
+        fibonacci_sequence = [0, 1]  # Initialize the Fibonacci sequence with the first two terms
+        for _ in range(2, n):
+            next_term = fibonacci_sequence[-1] + fibonacci_sequence[-2]  # Calculate the next term
+            fibonacci_sequence.append(next_term)  # Add the next term to the sequence
+        return fibonacci_sequence
+
+# Get the number of terms from the user
+num_terms = int(input("Enter the number of terms: "))
+
+# Generate the Fibonacci sequence
+fibonacci_sequence = fibonacci(num_terms)
+
+# Print the Fibonacci sequence
+print("The Fibonacci sequence up to term {} is:".format(num_terms))
+print(fibonacci_sequence)
+
+
+
+
+
+# Conditional
+
+def main():
+    # Prompt the user to enter their age
+    age = int(input("Enter your age: "))
+    
+    # Check if the age is greater than or equal to 18
+    if age >= 18:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")
+
+if __name__ == "__main__":
+    main()
+    
+    
+
