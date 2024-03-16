5abe24073ab85cba715fb45f606c4832b40a426d
SQCodes <paulsqmagadi@gmail.com>
2024-03-16 14:39:06 +0300

    new file:   hackerthon_March_16_2024/dart/functions.dart 	renamed:    hackerthon_March_16_2024/dart/hackerthon_dart1.dart -> hackerthon_March_16_2024/dart/program1.dart

---
+++ hackerthon_March_16_2024/dart/functions.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Perform addition
+  print("Result of addition: ${addition(5, 3)}");
+
+  // Perform multiplication
+  print("Result of multiplication: ${multiplication(5, 3)}");
+}
+
+// Function to perform addition
+int addition(int a, int b) {
+  return a + b;
+}
+
+// Function to perform multiplication
+int multiplication(int a, int b) {
+  return a * b;
+}
