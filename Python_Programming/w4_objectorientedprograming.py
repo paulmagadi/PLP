56cc38d9be647bf77fc46838d595e2be7ac6d19d
SQCodes <paulsqmagadi@gmail.com>
2024-04-02 11:57:16 +0300

    modified:   Python_Programming/w4_objectorientedprograming.py

--- Python_Programming/w4_objectorientedprograming.py
+++ Python_Programming/w4_objectorientedprograming.py
@@ -1,3 +1,13 @@
+# Create a Python class named Person.
+# The Person class should have the following attributes:
+# name: representing the person's name.
+# age: representing the person's age.
+# gender: representing the person's gender.
+# Implement a method called introduce that prints a message introducing the person with their name, age, and gender.
+# Create an instance of the Person class and call the introduce method to display the person's information.
+# Create a GitHub repository for your assignment and submit the link.
+
+
 class Person:
     def _init_(self, name, age, gender):
         self.name = name
@@ -8,7 +18,7 @@ class Person:
         print(f"Hello, my name is {self.name}. I am {self.age} years old and I am {self.gender}.")
 
 # Creating an instance of the Person class
-person1 = Person("John", 30, "male")
+person1 = Person("Paul", 25, "male")
 
 # Calling the introduce method to display the person's information
 person1.introduce()
