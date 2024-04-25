b6f0b53538949e8901c51accb2812de978baf6cd
SQCodes <paulsqmagadi@gmail.com>
2024-04-25 13:09:34 +0300

    new file:   flutter/flutter_ui_assignment/lib/home_page.dart

---
+++ flutter/flutter_ui_assignment/lib/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final List<String> items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+            onTap: () {
+              // Handle item selection
+              print('Selected: ${items[index]}');
+            },
+          );
+        },
+      ),
+    );
+  }
+}
