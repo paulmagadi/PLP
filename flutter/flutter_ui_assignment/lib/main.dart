1b3f33a3dcb6518527e157ccaf31013a6901a45a
SQCodes <paulsqmagadi@gmail.com>
2024-04-25 12:54:38 +0300

    modified:   flutter/flutter_ui_assignment/lib/main.dart

--- flutter/flutter_ui_assignment/lib/main.dart
+++ flutter/flutter_ui_assignment/lib/main.dart
@@ -5,7 +5,7 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset('assets/logo.png'),
+        child: Image.asset('assets/sqlogo.jpg'),
       ),
     );
   }
