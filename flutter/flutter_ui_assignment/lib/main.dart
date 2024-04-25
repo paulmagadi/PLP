ce358d00c4e5884ad4754be0fc59d113a1ad0221
SQCodes <paulsqmagadi@gmail.com>
2024-04-25 12:59:37 +0300

    modified:   flutter/flutter_ui_assignment/lib/main.dart

--- flutter/flutter_ui_assignment/lib/main.dart
+++ flutter/flutter_ui_assignment/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp(const SplashScreen());
+}
+
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset('assets/sqlogo.jpg'),
-      ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar (
+          title: const Center(
+            child: Text("Hello"),
+          ),
+        ),
+    ),
     );
   }
 }
