be8cc86a5a52e4d38abe55780f092f80dfd6cf08
SQCodes <paulsqmagadi@gmail.com>
2024-04-26 11:30:56 +0300

    modified:   DART-and FLUTTER-HACKATHON-2/to_do_app/lib/screens/home.dart

--- DART-and FLUTTER-HACKATHON-2/to_do_app/lib/screens/home.dart
+++ DART-and FLUTTER-HACKATHON-2/to_do_app/lib/screens/home.dart
@@ -5,7 +5,7 @@ import '../constants/colors.dart';
 import '../widgets/todo_item.dart';
 
 class Home extends StatefulWidget {
-  Home({Key? key}) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
