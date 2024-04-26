3643a16e0590c8837ed76c8753129b212ac89299
SQCodes <paulsqmagadi@gmail.com>
2024-04-26 11:56:26 +0300

    modified:   DART-and FLUTTER-HACKATHON-2/to_do_app/lib/model/todo.dart

--- DART-and FLUTTER-HACKATHON-2/to_do_app/lib/model/todo.dart
+++ DART-and FLUTTER-HACKATHON-2/to_do_app/lib/model/todo.dart
@@ -11,12 +11,12 @@ class ToDo {
 
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'Morning Excercise', isDone: true ),
-      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true ),
-      ToDo(id: '03', todoText: 'Check Emails', ),
-      ToDo(id: '04', todoText: 'Team Meeting', ),
-      ToDo(id: '05', todoText: 'Work on mobile apps for 2 hour', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
+      ToDo(id: '01', todoText: 'Morning Excercise. Daily 3', isDone: true ),
+      ToDo(id: '02', todoText: 'Call my Mother', isDone: true ),
+      ToDo(id: '03', todoText: 'Team Meeting with my Group', ),
+      ToDo(id: '04', todoText: 'Work on flutter mobile apps', ),
+      ToDo(id: '05', todoText: 'PowerHarks Hackathon', ),
+      ToDo(id: '06', todoText: 'Finish My Django Project', ),
     ];
   }
 }
