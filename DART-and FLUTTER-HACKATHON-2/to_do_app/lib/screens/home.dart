d20604e3cfd70342c22d58b99766f8ca6a5f2f04
SQCodes <paulsqmagadi@gmail.com>
2024-04-26 11:25:40 +0300

    modified:   DART-and FLUTTER-HACKATHON-2/to_do_app/lib/screens/home.dart

--- DART-and FLUTTER-HACKATHON-2/to_do_app/lib/screens/home.dart
+++ DART-and FLUTTER-HACKATHON-2/to_do_app/lib/screens/home.dart
@@ -210,7 +210,7 @@ class _HomeState extends State<Home> {
           width: 40,
           child: ClipRRect(
             borderRadius: BorderRadius.circular(20),
-            child: Image.asset('assets/images/avatar.jpeg'),
+            child: Image.asset('assets/images/paul.jpg'),
           ),
         ),
       ]),
