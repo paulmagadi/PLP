1ce45c93e90ca099863c9368c2e3516f97d212f9
SQCodes <paulsqmagadi@gmail.com>
2024-04-27 10:06:36 +0300

    modified:   DART-and FLUTTER-HACKATHON-2/to_do_app/lib/screens/home.dart

--- DART-and FLUTTER-HACKATHON-2/to_do_app/lib/screens/home.dart
+++ DART-and FLUTTER-HACKATHON-2/to_do_app/lib/screens/home.dart
@@ -324,23 +324,26 @@ class _HomeState extends State<Home> {
   }
 
   void _setSelectedCategory(TaskCategory category, BuildContext context) {
-    // Close the drawer after selection
-    Navigator.pop(context);
-    
-    // Filter the todo list based on the selected category
-    List<ToDo> filteredTodos;
-    if (category == TaskCategory.all) {
-        filteredTodos = todosList;
-    } else if (category == TaskCategory.completed) {
-        filteredTodos = todosList.where((todo) => todo.isDone).toList();
-    } else if (category == TaskCategory.pending) {
-        filteredTodos = todosList.where((todo) => !todo.isDone).toList();
-    }
-    
-    // Update the state with the filtered list of todo items
-    setState(() {
-        _foundToDo = filteredTodos;
-    });
-}
+    void _setSelectedCategory(TaskCategory category, BuildContext context) {
+      // Close the drawer after selection
+      Navigator.pop(context);
 
+      // Initialize filteredTodos
+      List<ToDo> filteredTodos = [];
+
+      // Filter the todo list based on the selected category
+      if (category == TaskCategory.all) {
+        filteredTodos = todosList;
+      } else if (category == TaskCategory.completed) {
+        filteredTodos = todosList.where((todo) => todo.isDone).toList();
+      } else if (category == TaskCategory.pending) {
+        filteredTodos = todosList.where((todo) => !todo.isDone).toList();
+      }
+
+      // Update the state with the filtered list of todo items
+      setState(() {
+        _foundToDo = filteredTodos;
+      });
+    }
+  }
 }
