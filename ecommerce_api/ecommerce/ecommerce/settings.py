ee420c64216cdba71729c65c380ed82ceb594dda
SQCodes <paulsqmagadi@gmail.com>
2024-05-22 14:57:13 +0300

    modified:   ecommerce_api/ecommerce/ecommerce/settings.py 	new file:   ecommerce_api/ecommerce/store/serializers.py

--- ecommerce_api/ecommerce/ecommerce/settings.py
+++ ecommerce_api/ecommerce/ecommerce/settings.py
@@ -119,8 +119,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = 'media/' 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
---
+++ ecommerce_api/ecommerce/store/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from .models import Product, Category, Specification
+from rest_framework import serializers
+
+
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        
+class SpecificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Specification
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        depth = 1
+
